41de9d966c223d258395a2f3272b0ced56867eeb
BlossomWhale <85476751+BlossomWhale@users.noreply.github.com>
2021-07-16 18:22:30 +0900

    add "h2 database dependency" add "jpa dependency"

--- src/test/java/com/spring/practice_01/web/HelloControllerTest.java
+++ src/test/java/com/spring/practice_01/web/HelloControllerTest.java
@@ -9,9 +9,10 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import static org.hamcrest.Matchers.is;
 
 @ExtendWith(SpringExtension.class)
 @WebMvcTest(controllers = HelloController.class)
@@ -20,7 +21,7 @@ public class HelloControllerTest {
     @Autowired
     private MockMvc mvc;
 
-    @Test
+//    @Test // 더 이상 진행 될 수 없는 테스트 이므로 폐기 됌.
     @DisplayName("name 잘 보이냐?")
     public void test1() throws Exception{
         String hello = "hello";
@@ -28,5 +29,17 @@ public class HelloControllerTest {
         mvc.perform(get("/hello"))
                 .andExpect(status().isOk())
                 .andExpect(content().string(hello));
+    }
+
+    @Test
+    @DisplayName("파라미터-리턴 테스트")
+    public void test2() throws Exception{
+        String name = "hello";
+        int amount = 1000;
+
+        mvc.perform(get("/hello").param("name",name).param("amount", String.valueOf(amount)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name",is(name)))
+                .andExpect(jsonPath("$.amount",is(amount)));
     }
 }
